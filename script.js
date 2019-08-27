573041e7e7a85ee0da58ff11af2b64be14366d55
Sam <super8989@gmail.com>
2019-08-26 22:02:11 -0400

    event listener to multiple elements

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const button = document.querySelector("button");
-const bark = new Audio('sound/bark.mp3');
+const animals = document.querySelectorAll(".animal");
 
-button.addEventListener("click", function() {
-    let duration = bark.duration;
-    let muted = bark.muted;
-    bark.play();
-    console.log(muted);
-})
+console.log(animals);
+
+for (let i=0; i < animals.length; i++) {
+    animals[i].addEventListener("click", function() {
+        let animal = this.innerHTML;
+        console.log(animal);
+    })
+}
