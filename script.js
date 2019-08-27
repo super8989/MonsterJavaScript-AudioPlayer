d0ebf2897910013b43a8423a2d9a666022344387
Sam <super8989@gmail.com>
2019-08-26 22:10:22 -0400

    sound selector on click

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ console.log(animals);
 for (let i=0; i < animals.length; i++) {
     animals[i].addEventListener("click", function() {
         let animal = this.innerHTML;
-        console.log(animal);
+        let lowerAnimal = animal.toLowerCase();
+        makeSound(lowerAnimal);
     })
+}
+
+function makeSound(name) {
+    console.log(name);
+
+    switch(name) {
+        case "lion":
+            let sound1 = new Audio("sound/lion.mp3");
+            sound1.play();
+            break;
+        
+        case "cougar":
+            let sound2 = new Audio("sound/cougar.mp3");
+            sound2.play();
+            break;
+
+        case "dog":
+            let sound3 = new Audio("sound/bark.mp3");
+            sound3.play();
+            break;            
+    }
 }
